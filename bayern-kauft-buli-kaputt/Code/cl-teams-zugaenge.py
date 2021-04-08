1b1f8181ed9d84fe74311c3f2ba0b7ce11cfe82a
Marvinho <Marvin.Springer@web.de>
2021-04-08 12:12:27 +0200

    formatation of marktwert,ablöse

--- bayern-kauft-buli-kaputt/Code/cl-teams-zugaenge.py
+++ bayern-kauft-buli-kaputt/Code/cl-teams-zugaenge.py
@@ -6,30 +6,62 @@ Created on Wed Apr  7 21:10:02 2021
 """
 import pandas as pd
 import numpy as np
+from pathlib import Path
+import os
 
-df = pd.read_csv("buli-cl-teams-zugaenge.csv")
+abs_path = "C:/Users/Marvin/Desktop/Portfolio/soccer/bayern-kauft-buli-kaputt/buli-cl-teams-zugaenge.csv"
+# cwd = os.getcwd()
+# file_path = (Path(cwd) / "../test.csv").resolve()
+# print(cwd, file_path)
+
+df = pd.read_csv(abs_path)
 
 print(df)
 
 def deal_with_vereinslos(df):
-    # df["Aufnehmender-Verein"] = np.where(df["Abgebende-Liga"] == "-", df['col2'], df['col1'])
-    # df["Aufnehmender-Verein"].replace(nan,)
-    pass
+    if(df.isnull().values.any()):
+        df["Aufnehmender-Verein"] = np.where(df["Abgebende-Liga"] == "-", df["Saison"], df["Aufnehmender-Verein"])
+        df["Saison"] = np.where(df["Abgebende-Liga"] == "-", df["Ablöse"], df["Saison"])
+        df["Ablöse"] = np.where(df["Abgebende-Liga"] == "-", "-", df["Ablöse"])
+    else:
+        print("Already dealt with clubless players")
+    # df["Aufnehmender-Verein"].replace("-",df["Saison"],inplace=True)
+    # df["Saison"]replace("-",df["Ablöse"],inplace=True)
+
+
 
 def split_abloese_to_transferart(df):
-    leih_condition = df["Ablöse"].str.contains("Leih")
-    print(leih_condition)
-    intern_transfer_condition = df["Ablöse"] == "-"
-    conditions = [leih_condition, intern_transfer_condition]
-    choices = ["Leihe", "Interner Wechsel"]
-    df["Transferart"] = np.select(conditions, choices, default="Transfer")
+    if("Transferart" not in df.columns):
+        leih_condition = df["Ablöse"].str.contains("Leih")
+        # print(leih_condition)
+        intern_transfer_condition = df["Ablöse"] == "-"
+        conditions = [leih_condition, intern_transfer_condition]
+        choices = ["Leihe", "Interner Wechsel"]
+        df["Transferart"] = np.select(conditions, choices, default="Transfer")
+    else:
+        print("Already splitted to transferart")
 
-# format_abloese(df)
+
 
 def format_abloese_to_int(df):
-    df["Leihkosten"] = df["Ablöse"]
-    df.loc[df["Transferart"] != "Leihe", "Leihkosten"] = 0
-    df.loc[df["Transferart"] == "Leihe", "Transferart"] = 0
-    df.loc[df["Ablöse"] == "-", "Transferart"] = 0
+    if(df["Ablöse"].dtype != int):
+        df["Leihkosten"] = df["Ablöse"]
+        df["Leihkosten"] = np.where(df["Transferart"] != "Leihe", "-", df["Leihkosten"])
+        # df["Ablöse"] = np.where(df["Transferart"] == "Leihe", 0, df["Ablöse"])
+        mask = ((df["Transferart"] == "Leihe") |
+                (df["Ablöse"] == "-") | 
+                (df["Ablöse"] == "ablösefrei") |
+                (df["Ablöse"] == "?"))
+        df.loc[mask, "Ablöse"] = 0
+        df["Ablöse"] = df["Ablöse"].str.replace(",","")
+        df["Ablöse"] = df["Ablöse"].str.replace(" Mio. €","0000")
+        df["Ablöse"] = df["Ablöse"].str.replace(" Tsd. €","000")
+        df["Ablöse"].fillna(0, inplace=True)
+        df["Ablöse"] = df["Ablöse"].astype(int)
+    else:
+        print("Already formatted the Ablöse")
     
-    
+deal_with_vereinslos(df)  
+split_abloese_to_transferart(df) 
+format_abloese_to_int(df)
+df["Ablöse"].dtype == int
